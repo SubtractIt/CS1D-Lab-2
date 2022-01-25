21503784484d88cf4143b60282dc154efd0e1251
adit <aditmohindra@gmail.com>
2022-01-24 16:26:14 -0800

    Update main.cpp

--- Part 1/main.cpp
+++ Part 1/main.cpp
@@ -1,16 +1,20 @@
-// Author Your Name
+// Danny Cowles and Adit Mohindra
 // Description: Hello World in C++
 //============================================================
 #include <iostream>
+#include <string>
+
 using namespace std;
 int hello1Function(string name1);
 int hello2Function(string name2);
 
 int main()
 {
+    
     string name1="your name1";
     hello1Function(name1);
     string name2="your name2";
     hello2Function(name2);
+
     return 0;
 }
